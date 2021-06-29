a85a97068f9dcab3f757c01aeb586abc775aa79e
AudreyKK <aukkeefe@gmail.com>
2021-06-29 10:39:43 -0400

    while do-while loops

--- Week1/src/exampleOne/ExampleOne.java
+++ Week1/src/exampleOne/ExampleOne.java
@@ -3,12 +3,16 @@ package exampleOne;
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		boolean on = true;
+		boolean on = false;
 		
 		while(on) {
 			System.out.println("Inside the while loop");
 			//on = false;
 		}
+		
+		do {
+			System.out.println("Inside the do-while loop");
+		} while (on);
 
 	}
 
