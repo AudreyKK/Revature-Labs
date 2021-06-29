0082b0e34b73f3f82736ccbcdc87e6a03c22438e
AudreyKK <aukkeefe@gmail.com>
2021-06-29 18:03:40 -0400

    composite classes

---
+++ Week1/src/classA/ClassA.java
@@ -0,0 +1,20 @@
+package classA;
+
+public class ClassA {
+	
+	String name = "name";
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
