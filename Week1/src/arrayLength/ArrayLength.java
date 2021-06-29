c170c5920c2a95c92e0a7913892e2695aa13fcbc
AudreyKK <aukkeefe@gmail.com>
2021-06-29 10:14:29 -0400

    array length

---
+++ Week1/src/arrayLength/ArrayLength.java
@@ -0,0 +1,14 @@
+package arrayLength;
+
+public class ArrayLength {
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		// creates the same thing as 
+		// int[] arr = int[10];
+		System.out.println(arr.length);
+		
+
+	}
+
+}
