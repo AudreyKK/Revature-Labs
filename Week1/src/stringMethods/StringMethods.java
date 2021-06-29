d464a7ddb5f9cd5cbdde88c227a56903fd383776
AudreyKK <aukkeefe@gmail.com>
2021-06-29 17:44:22 -0400

    string methods

---
+++ Week1/src/stringMethods/StringMethods.java
@@ -0,0 +1,20 @@
+package stringMethods;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		String s = new String("Hello");
+		
+		// equals()
+		System.out.println(str.equals(s));
+		
+		// length()
+		System.out.println(str.length());
+		
+		// indexOf()
+		System.out.println(str.indexOf("H"));
+
+	}
+
+}
