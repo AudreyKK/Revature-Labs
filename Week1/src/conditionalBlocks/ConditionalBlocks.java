c1992e5a9a7215e5ac881cf3e3977f4bae4751b5
AudreyKK <aukkeefe@gmail.com>
2021-06-28 21:10:45 -0400

    conditional blocks

---
+++ Week1/src/conditionalBlocks/ConditionalBlocks.java
@@ -0,0 +1,17 @@
+package conditionalBlocks;
+
+public class ConditionalBlocks {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		
+		if (b) {
+			System.out.println("Inside the if statement");
+			
+		}
+		
+		System.out.println("Outside the if statement");
+
+	}
+
+}
