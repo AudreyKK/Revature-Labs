da8590ccbc63c09bc1a616973f5e990767539581
AudreyKK <aukkeefe@gmail.com>
2021-06-28 21:21:46 -0400

    conditional blocks2: electric boogaloo

--- Week1/src/conditionalBlocks/ConditionalBlocks.java
+++ Week1/src/conditionalBlocks/ConditionalBlocks.java
@@ -3,14 +3,25 @@ package conditionalBlocks;
 public class ConditionalBlocks {
 
 	public static void main(String[] args) {
-		boolean b = true;
+		boolean firstCondition = false;
+		boolean secondCondition = true;
 		
-		if (b) {
-			System.out.println("Inside the if statement");
+		if (firstCondition) {
+			System.out.println("Inside the if-statement");
+			
+			if (secondCondition) {
+				System.out.println("inside the nested if-statement");
+			}
+			
+		}else if (secondCondition) { 
+			System.out.println(5);
+			
+		}else {
+			System.out.println("Inside the else-statement");
 			
 		}
 		
-		System.out.println("Outside the if statement");
+		System.out.println("Outside the if-statement");
 
 	}
 
