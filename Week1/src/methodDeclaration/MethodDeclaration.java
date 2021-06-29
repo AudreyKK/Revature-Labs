9e02ea806b86c234d0356ac01e9ad5655128d6c5
AudreyKK <aukkeefe@gmail.com>
2021-06-29 13:04:37 -0400

    methods

---
+++ Week1/src/methodDeclaration/MethodDeclaration.java
@@ -0,0 +1,17 @@
+package methodDeclaration;
+
+public class MethodDeclaration {
+
+	public static void main(String[] args) {
+		MethodDeclaration md = new MethodDeclaration();
+		md.talk();
+		md.talk();
+		md.talk();
+
+	}
+	
+	public void talk() {
+		System.out.println("Inside the talk method");
+	}
+	
+}
