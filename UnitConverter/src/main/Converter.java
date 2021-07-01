009f11a049a35bb55ce4c96aa0c1ba3e0d80ed9b
AudreyKK <aukkeefe@gmail.com>
2021-07-01 12:12:14 -0400

    weights menu

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Converter {
+	static double poundsToKilogramsFactor = 2.20462;
 	
 	public static void mainMenu() {
 		System.out.println("What kind of conversion would you like to make?");
@@ -46,16 +47,57 @@ public class Converter {
 		return resp;
 	}
 
+	
+	// 
+	public static void volumeMenu() {
+		System.out.println("todo");
+	}
+	
+	//
+	public static void distanceMenu() {
+		System.out.println("todo");
+	}
+	
+	// 
+	public static void currencyMenu() {
+		System.out.println("todo");
+	}
+	
+	
+	//
+	public static double pounds2kilos (String amount) {
+		
+		return Double.parseDouble(amount) * poundsToKilogramsFactor;
+	}
+	
 	//
 	public static void weightsMenu() {
+		String input;
+		
 		System.out.println("Select what you want to convert: ");
-		System.out.println("1. ");
+		System.out.println("1. kg to pounds");
+		System.out.println("2. oz to grams");
+		System.out.println("3. Interplanetary weight conversion");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextLine();
+		
+		try {
+			switch(input) {
+				case "1": pounds2Kilos();
+			}
+		}catch (NoSuchElementException ex){
+			System.out.println("Please enter a valid number");
+			sc.nextLine();
+		}
 	}
 	
 	
 	// Main loop
 	public static void main(String[] args) {
 		boolean running = true;
+		int input;
 		
 		
 		while(running) {
@@ -64,7 +106,32 @@ public class Converter {
 			// Selection Menu
 			mainMenu();
 			// Verify input is convertible from string to int
-			validateInput();
+			input = validateInput();
+			
+			switch(input) {
+				
+				case 1: { 
+					weightsMenu();
+					break;
+				}
+				
+				case 2: {
+					volumeMenu();
+					break;
+				}
+				
+				case 3:{
+					distanceMenu();
+					break;
+				}
+				
+				case 4: {
+					currencyMenu();
+					break;
+				}
+				
+				case 5: break;
+			}
 			
 
 			running = false;
