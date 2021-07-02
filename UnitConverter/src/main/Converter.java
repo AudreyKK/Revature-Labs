58d7e8675b02d3e3264d21bc26a861e29e2f7a4c
AudreyKK <aukkeefe@gmail.com>
2021-07-02 14:34:31 -0400

    first draft done

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Converter {
+		
 	static double poundsToKilogramsFactor = 2.20462;
 	static double ouncesToGramsFactor = 28.3495;
 	static double gallonsToLitersFactor = 3.78541;
@@ -13,7 +14,19 @@ public class Converter {
 	static double metersToFeetFactor = 3.28084;
 	static double milesToNauticalMilesFactor = 0.86897626970789;
 	static double poundsToDollarsFactor = 1.38;
+	static double dollarsToPesosFactor = 19.85;
+	static double dollarsToRupeesFactor = 74.52;
 	
+	// Planets' Gravity Factors
+	static double mercury = 0.378;
+	static double venus = 0.907;
+	static double moon = 0.166;
+	static double mars = 0.377;
+	static double jupiter = 2.36;
+	static double saturn = 0.916;
+	static double uranus = 0.889;
+	static double neptune = 1.12;
+	static double pluto = 0.071;
 	
 	static final Scanner sc = new Scanner(System.in);
 	
@@ -21,6 +34,85 @@ public class Converter {
 		return sc.nextDouble();
 	}
 	
+	public static String collectUnits () {
+		boolean deciding = true;
+		String returnString = "";
+		
+		while (deciding) {
+			System.out.println("\n First, are you using kilograms or pounds?\n");
+			System.out.println("1. kg");
+			System.out.println("2. lb");
+			int input = sc.nextInt();
+		if (input == 1) {
+			returnString = "kg";
+			deciding = false;
+		}else if (input == 2) {
+			returnString = "lb";
+			deciding = false;
+		}else {
+			System.out.println("Please type a valid input.");
+		}
+		
+		}
+		
+		return returnString;
+		
+	}
+	
+	public static void earthToMercury(double weight, String unit) {
+		double result = weight * mercury;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Mercury.");
+	}
+	
+	public static void earthToVenus(double weight, String unit) {
+		double result = weight * venus;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Venus.");
+	}
+	
+	public static void earthToMoon(double weight, String unit) {
+		double result = weight * moon;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on the Moon.");
+	}
+	
+	public static void earthToMars(double weight, String unit) {
+		double result = weight * mars;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Mars.");
+	}
+	
+	public static void earthToJupiter(double weight, String unit) {
+		double result = weight * jupiter;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Jupiter.");
+	}
+	
+	public static void earthToSaturn(double weight, String unit) {
+		double result = weight * saturn;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Saturn.");
+	}
+	
+	public static void earthToUranus(double weight, String unit) {
+		double result = weight * uranus;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Uranus.");
+	}
+	
+	public static void earthToNeptune(double weight, String unit) {
+		double result = weight * neptune;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Neptune.");
+	}
+	
+	public static void earthToPluto(double weight, String unit) {
+		double result = weight * pluto;
+		System.out.println("" + weight + " " + unit + " on Earth is equivalent to ");
+		System.out.println("" + result + " " + unit + " on Pluto.");
+	}
+
 	public static void poundsToKilos(double pounds) {
 		double result = pounds * poundsToKilogramsFactor;
 		System.out.println("\n" + pounds + " pound(s) converts to ");
@@ -70,7 +162,21 @@ public class Converter {
 	}
 	
 	public static void poundsToDollars (double pounds) {
-		
+		double result = pounds * poundsToDollarsFactor;
+		System.out.println("\n" + pounds + " pound(s) converts to ");
+		System.out.println("" + result + " dollar(s).\n");
+	}
+	
+	public static void dollarsToPesos (double dollars) {
+		double result = dollars * dollarsToPesosFactor;
+		System.out.println("\n" + dollars + " dollar(s) converts to ");
+		System.out.println("" + result + " peso(s).\n");
+	}
+	
+	public static void dollarsToRupees (double dollars) {
+		double result = dollars * dollarsToRupeesFactor;
+		System.out.println("\n" + dollars + " dollar(s) converts to ");
+		System.out.println("" + result + " rupees.\n");
 	}
 	
 	public static void mainMenu() {
@@ -134,6 +240,7 @@ public class Converter {
 				System.out.println("Please enter the quanitity you want converted");
 				double resp = collectQuantity();
 				galToLiter(resp);
+				break;
 	
 			}
 			case "3": {
@@ -178,9 +285,9 @@ public class Converter {
 	// 
 	public static void currencyMenu() {
 		System.out.println("Select what you want to convert: ");
-		System.out.println("1. Miles to kilometers");
-		System.out.println("2. Meters to feet");
-		System.out.println("3. Miles to nautical miles");
+		System.out.println("1. British pounds to US dollars");
+		System.out.println("2. US Dollars to Mexican Pesos");
+		System.out.println("3. US Dollars to Indian Rupees");
 		
 		sc.nextLine(); // get rid of lingering \n if present.
 		String input = sc.nextLine();
@@ -207,6 +314,69 @@ public class Converter {
 		}
 	}
 	
+	//
+	public static void spaceMenu() {
+		int input;
+		double mass;
+		
+		System.out.println("\nSelect an option to see how gravity affects weight");
+		System.out.println("on other planets.\n");
+		System.out.println("1. Mercury");
+		System.out.println("2. Venus");
+		System.out.println("3. The moon");
+		System.out.println("4. Mars");
+		System.out.println("5. Jupiter");
+		System.out.println("6. Saturn");
+		System.out.println("7. Uranus");
+		System.out.println("8. Neptune");
+		System.out.println("9. Pluto");
+		
+		input = sc.nextInt();
+		
+		String unit = collectUnits();
+		
+		System.out.println("Input the quantity of mass you're converting.");
+		sc.nextLine();
+		mass = sc.nextDouble();
+		
+		switch(input) {
+			case 1: {
+				earthToMercury(mass, unit);
+				break;
+			}
+			case 2: {
+				earthToVenus(mass, unit);
+				break;
+			}
+			case 3: {
+				earthToMoon(mass, unit);
+				break;
+			}
+			case 4: {
+				earthToMars(mass, unit);
+				break;
+			}
+			case 5: {
+				earthToJupiter(mass, unit);
+				break;
+			}
+			case 6: {
+				earthToSaturn(mass, unit);
+				break;
+			}
+			case 7: {
+				earthToUranus(mass, unit);
+				break;
+			}
+			case 8: {
+				earthToNeptune(mass, unit);
+				break;
+			}
+			case 9: {
+				earthToPluto(mass, unit);
+			}
+		}
+	}
 	
 	//
 	public static void weightsMenu() {
@@ -217,7 +387,7 @@ public class Converter {
 		System.out.println("2. oz to grams");
 		System.out.println("3. Interplanetary weight conversion");
 		
-		sc.next(); // get rid of lingering \n if present.
+		sc.nextLine(); // get rid of lingering \n if present.
 		input = sc.nextLine();
 		
 		switch(input) {
@@ -231,7 +401,11 @@ public class Converter {
 				System.out.println("Please enter the quanitity you want converted");
 				double resp = collectQuantity();
 				ozToGrams(resp);
+				break;
 	
+			}
+			case "3": {
+				spaceMenu();
 			}
 		}
 		
